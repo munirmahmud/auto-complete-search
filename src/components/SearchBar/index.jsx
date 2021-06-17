c368f6fe386b1d50f4929ed4eb338a76e70278a1
Munir Mahmud <munirmahmud6@gmail.com>
2021-06-17 08:08:22 +0600

    Expand & collapse animation added

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useClickOutside } from "react-click-outside-hook";
 import { IoClose, IoSearch } from "react-icons/io5";
 import {
   CloseIcon,
@@ -8,15 +9,58 @@ import {
   SearchInputContainer,
 } from "./searchbar.styles";
 
+const containerVariants = {
+  expanded: {
+    height: "20rem",
+  },
+  collapsed: {
+    height: "3.9rem",
+  },
+};
+
+const containerTransition = {
+  type: "spring",
+  damping: 22,
+  stiffness: 150,
+};
+
 const SearchBar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [parentRef, isClickedOutside] = useClickOutside();
+  const inputRef = useRef();
+
+  const expandContainer = () => {
+    setIsExpanded(true);
+  };
+
+  const collapseContainer = () => {
+    setIsExpanded(false);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  useEffect(() => {
+    if (isClickedOutside) collapseContainer();
+  }, [isClickedOutside]);
+
   return (
-    <SearchBarContainer>
+    <SearchBarContainer
+      animate={isExpanded ? "expanded" : "collapsed"}
+      variants={containerVariants}
+      transition={containerTransition}
+      ref={parentRef}
+    >
       <SearchInputContainer>
         <SearchIcon>
           <IoSearch />
         </SearchIcon>
-        <SearchInput placeholder="Search for TV shows..." />
-        <CloseIcon>
+        <SearchInput
+          placeholder="Search for TV shows..."
+          onFocus={expandContainer}
+          ref={inputRef}
+        />
+        <CloseIcon onClick={collapseContainer}>
           <IoClose />
         </CloseIcon>
       </SearchInputContainer>
--- src/components/SearchBar/searchbar.styles.js
+++ src/components/SearchBar/searchbar.styles.js
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const SearchBarContainer = styled.div`
+export const SearchBarContainer = styled(motion.div)`
   display: flex;
   flex-direction: column;
   width: 34rem;
